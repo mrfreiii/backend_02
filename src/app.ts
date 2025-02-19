30bbaea32511e26c2e3b44ccb8eeb17119dcc451
Igor_Titov <mrfreiii@gmail.com>
2025-02-19 18:52:54 +0300

    added response for /

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,10 @@ export const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/", (req, res)=>{
+    res.json("server is available")
+})
+
 app.use(SETTINGS.PATH.BLOGS, blogsRouter);
 app.use(SETTINGS.PATH.POSTS, postsRouter);
 app.use(SETTINGS.PATH.USERS, usersRouter);
